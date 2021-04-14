4ab2be134b4f119434bc74b628c47fd22c8a8c8b
Fremen1990 <fremen1990@gmail.com>
2021-04-14 14:22:53 +0200

    react firts commit in new

---
+++ src/components/MenuItems.js
@@ -0,0 +1,25 @@
+export const MenuItems = [
+  {
+    title: "News",
+    path: "/news",
+    cName: "dropdown-link",
+  },
+
+  {
+    title: "Society",
+    path: "/society",
+    cName: "dropdown-link",
+  },
+
+  {
+    title: "Science",
+    path: "/science",
+    cName: "dropdown-link",
+  },
+
+  {
+    title: "Criminal",
+    path: "/criminal",
+    cName: "dropdown-link",
+  },
+];
---
+++ src/components/Navbar.js
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import Dropdown from "./Dropdown";
+import logo from "../assets/ANON _ Edyta LOGO2 białe kółko - small.png";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const [dropdown, setDropDown] = useState(false);
+
+  const handleClick = () => {
+    setClick(!click);
+  };
+
+  const closeMobileMenu = () => {
+    setClick(false);
+  };
+
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropDown(false);
+    } else {
+      setDropDown(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropDown(false);
+    } else {
+      setDropDown(false);
+    }
+  };
+
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="navbar-logo">
+          ANON <img className="navbar__logo" src={logo} alt="Logo" />
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+
+          <li
+            className="nav-item"
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            <Link
+              to="/articles"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Articles <i className="fas fa-caret-down" />
+            </Link>
+            {dropdown && <Dropdown />}
+          </li>
+
+          <li className="nav-item">
+            <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+              About
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link
+              to="/membership"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Membership
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link
+              to="/contact-us"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Contact Us
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link
+              t="/subscribe"
+              className="nav-links-mobile"
+              onClick={closeMobileMenu}
+            >
+              Subscribe
+            </Link>
+          </li>
+        </ul>
+        <Button />
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
