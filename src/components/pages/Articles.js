78a3518d74b88c7bd94369a7cd5324878bbd82d7
Fremen1990 <fremen1990@gmail.com>
2021-04-25 04:54:29 +0200

    Articles prototype

--- src/components/pages/Articles.js
+++ src/components/pages/Articles.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../styles/main.scss";
+import ArticlesCards from "../ArticlesCards";
 
 export default function Articles() {
   return (
     <>
       <h1 className="articles">
-        <p>Articles</p>
+        {/* <p>Articles</p> */}
+        <ArticlesCards />
       </h1>
     </>
   );
