ad203a2ecaa3ac25237e868ffb250bcccf7a36a6
Fremen1990 <fremen1990@gmail.com>
2021-11-21 19:13:29 +0100

    api correction

--- src/components/ShowImage.js
+++ src/components/ShowImage.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { API } from "../config";
-import { API_EXTERNAL } from "../config";
+// import { API_EXTERNAL } from "../config";
 
 const ShowImage = ({ item, url }) => (
     <div className="article-image ">
         <img
-            src={`${API_EXTERNAL}/${url}/photo/${item._id}`}
+            src={`${API}/${url}/photo/${item._id}`}
             alt={item.name}
             style={{ maxHeight: "100%", maxWidth: "100%" }
       }
